5fca518132ad5d4dbf8550e8e462ce814ff08f2f
Mike Stein <Mike@Stein.com>
2022-09-03 01:48:08 +0100

    installer

--- create_build.sh
+++ create_build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-cd OpenRA
+cd ../OpenRA
+rm -rf build/windows
+mkdir -p build/windows
+sudo make install version
 sudo ./packaging/windows/buildpackage.sh "vms" "${PWD}/build/windows"
+cd -
+cp ../OpenRA/build/windows/OpenRA-vms-x64.exe .
+
