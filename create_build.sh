bdd74c3b7c464f86bc619c867ea73a010c1c4555
Mike Stein <Mike@Stein.com>
2022-09-16 17:21:02 +0100

    add linux binary

--- create_build.sh
+++ create_build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 cd ../OpenRA
-rm -rf build/windows
-mkdir -p build/windows
+rm -rf build/{windows,linux}
+mkdir -p build/{windows,linux}
 sudo make install version
 sudo ./packaging/windows/buildpackage.sh "vms" "${PWD}/build/windows"
+sudo ./packaging/linux/buildpackage.sh "vms" "${PWD}/build/linux"
 cd -
 cp ../OpenRA/build/windows/OpenRA-vms-x64.exe .
+cp ../OpenRA/build/linux/OpenRA-Red-Alert-devel-x86_64.AppImage .
 
